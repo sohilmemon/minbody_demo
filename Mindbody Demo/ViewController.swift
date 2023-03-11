50f31f3d04cf7a2d917d88f06612deeb1c86cae0
Space <sohil.threat@gmail.com>
2023-03-11 09:33:32 +0530

    Initial Commit

---
+++ Mindbody Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mindbody Demo
+//
+//  Created by Space on 11/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
